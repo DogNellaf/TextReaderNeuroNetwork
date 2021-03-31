728155692a0c9e4449e713cb8986ad8738f85f5c
unknown <danil.sidorov16@gmail.com>
2021-03-31 13:50:23 +0300

    Базовый класс нейрона и перечисление для типов нейронов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NeuroNetwork
+namespace TextReaderNeiro
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
---
+++ Neuron.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Hidden)
+        {
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+
+    }
+}
